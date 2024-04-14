661a31e0ace0ba4b5ac4ed683cac56b3b7416eb0
syrine hentati <sirinehentati2084@gmail.com>
2024-04-14 12:47:29 +0100

    sticky navbar

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-
+  playerData: any = {
+    playerName: 'keranis',
+    playerEmail: 'sirinehentati@gmail.com',
+    playerProfession: 'Student',
+    playerBirthday: '2001-06-09',
+    AvatarURL:'https://readyplayerme',
+    Joke:'Why don t skeletons fight each other? They don t have the guts!'
+  };
+  UserData: any = {
+  FirstName:'Syrine',
+  LastName: 'Hentati',
+  email: 'sirinehentati@gmail.com',
+  password: '',
+  }
 }
